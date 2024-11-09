1d9d391fb8a4f56a74a8e6a640b8fc43a029ac42
Ashkan <the.epic.2077@gmail.com>
2024-11-09 12:04:32 +0330

    updating Onboarding.js

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,16 +1,27 @@
 import { El } from "../utils/create-element";
 
 export default function OnBoardingPage() {
-  const testing = El({
+  const logoIcon = El({
+    element: "img",
+    src: "./src/assets/image/logo-white.svg",
+    className: "w-[28.54px] h-[39.93px]",
+  });
+  const logo = El({
+    element: "div",
+    children: logoIcon,
+    className:
+      " bg-black rounded-full w-[59px] h-[59px] grid justify-center items-center",
+  });
+  const logoName = El({
     element: "h1",
-    children: "Testing this out",
-    className: "color-black ",
+    children: "Shoea",
+    className: "color-black font-bold text-logo font-sans ml-2",
   });
 
   const container = El({
     element: "div",
-    children: testing,
-    className: "w-screen h-screen grid justify-center items-center",
+    children: [logo, logoName],
+    className: "w-screen h-screen flex justify-center items-center",
   });
   return container;
 }
