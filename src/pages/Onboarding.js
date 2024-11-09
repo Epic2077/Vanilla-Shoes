eff854dded028bd023d8f047fb236bfebc669714
Ashkan <the.epic.2077@gmail.com>
2024-11-09 13:09:19 +0330

    updating Onboarding.js

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -22,10 +22,60 @@ export default function OnBoardingPage() {
     src: "./src/assets/image/spinner-atom.svg",
     className: "absolute bottom-[117px] animate-spin",
   });
-  const container = El({
+  let container = El({
     element: "div",
     children: [logo, logoName, loading],
     className: "w-screen h-screen flex justify-center items-center",
   });
-  return container;
+  let page = El({
+    element: "section",
+    children: container,
+  });
+
+  //   setTimeout(() => {
+  const newContent = newData();
+  page.innerHTML = newContent.innerHTML;
+  //   }, 5000);
+  return page;
+}
+
+function newData() {
+  const welcome = El({
+    element: "h2",
+    children: "Welcome to 👋",
+    className: "font-semibold text-[40px] text-white mb-[16px]",
+  });
+  const name = El({
+    element: "h1",
+    children: "Shoea",
+    className: "font-bold text-[72px] text-white mb-[28px]",
+  });
+  const des = El({
+    element: "p",
+    children:
+      "The best sneakers & shoes e-commerse app of the century for your fashion needs!",
+    className: "font-semibold text-white text-[16px]",
+  });
+  const content = El({
+    element: "div",
+    children: [welcome, name, des],
+    className: "grid ",
+  });
+  const backgroundFilter = El({
+    element: "div",
+    children: content,
+    className:
+      "bg-gradient-to-b from-[#00000000] to-[#000000CC] w-screen h-screen px-[32px] grid items-end pb-[74px]",
+  });
+  const background = El({
+    element: "div",
+    children: backgroundFilter,
+    className: "bg-[url('./src/assets/image/bg-1.png')] w-screen h-screen",
+  });
+  const newContent = El({
+    element: "div",
+    children: [background],
+    className: "text-center text-xl font-bold mt-5",
+  });
+  return newContent;
 }
