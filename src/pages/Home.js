e24958da406daa98533dcac65ef57ba120370b3f
Ashkan <the.epic.2077@gmail.com>
2024-11-10 23:41:32 +0330

    Updating Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,46 @@
+import { El } from "../utils/create-element";
+
 const HomePage = () => {
-  const container = document.createElement("div");
-  container.textContent = "Welcome to the Home Page!!";
-  return container;
+  const login = El({
+    element: "p",
+    children: "Login",
+    className: "text[#152536] text-[16px] font-bold",
+  });
+  const goodMorning = El({
+    element: "p",
+    children: "Good Morning 👋",
+    className: "text-[#757475] text-[16px] font-medium",
+  });
+  const pfpContent = El({
+    element: "div",
+    children: [goodMorning, login],
+    className: "grid ml-4",
+  });
+  const pfp = El({
+    element: "div",
+    children: [],
+    className:
+      "bg-[url('./src/assets/image/pfp.png')] w-[48px] h-[48px] rounded-full",
+  });
+  const profileData = El({
+    element: "div",
+    children: [pfp, pfpContent],
+    className: "flex",
+  });
+
+  const header = El({
+    element: "header",
+    children: [profileData],
+    className: "w-screen h-[80px] flex items-center",
+  });
+
+  const home = El({
+    element: "section",
+    children: [header],
+    className: "px-[24px] w-screen",
+  });
+
+  return home;
 };
 
 export default HomePage;
