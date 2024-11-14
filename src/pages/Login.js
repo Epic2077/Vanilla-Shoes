51c4c2c8b772d760a57842c7f8794ca163c23523
Ashkan <the.epic.2077@gmail.com>
2024-11-15 00:45:56 +0330

    Login in progress

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import { El } from "../utils/create-element";
 import { router } from "../routes/router";
 
 export default function login() {
+  document.title = "Login";
   const back = El({
     element: "img",
     src: "./src/assets/icons/back.svg",
@@ -22,11 +23,79 @@ export default function login() {
   const logo = El({
     element: "img",
     src: "./src/assets/icons/big-logo.svg",
-    className: "w-[54px] h-[81px] mt-[76px]",
+    className: "w-[54px] h-[81px] mt-[76px] mb-[118px] mx-auto",
+  });
+  const title = El({
+    element: "h1",
+    children: "Login to Your Account",
+    className: "font-semibold text-[32px] text-center",
+  });
+  const inputsEmail = El({
+    element: "div",
+    children: [
+      El({
+        element: "img",
+        src: "./src/assets/icons/email.svg",
+        className: "w-[14px] h-[10.5px]",
+      }),
+      El({
+        element: "input",
+        type: "email",
+        placeholder: "Email",
+        id: "email",
+        className:
+          "ml-[4px] bg-transparent h-[37px] w-full order-none outline-none",
+      }),
+    ],
+    className:
+      "w-full h-[37px] items-center flex bg-[#FAFAFA] px-[13px] rounded-[4px]",
+  });
+  const inputsPassword = El({
+    element: "div",
+    children: [
+      El({
+        element: "img",
+        src: "./src/assets/icons/lock.svg",
+        className: "w-[14px] h-[14px]",
+      }),
+      El({
+        element: "input",
+        type: "password",
+        placeholder: "Password",
+        id: "pass",
+        className:
+          "ml-[4px] bg-transparent h-[37px] w-full order-none outline-none",
+      }),
+      El({
+        element: "img",
+        src: "./src/assets/icons/eye.svg",
+        className: "cursor-pointer",
+        eventListener: [
+          {
+            event: "click",
+            callback: () => {
+              const passField = document.getElementById("pass");
+              if (passField.type === "password") {
+                passField.type = "text";
+              } else {
+                passField.type = "password";
+              }
+            },
+          },
+        ],
+      }),
+    ],
+    className:
+      "w-full h-[37px] items-center flex bg-[#FAFAFA] px-[13px] rounded-[4px]",
+  });
+  const form = El({
+    element: "form",
+    children: [inputsEmail, inputsPassword],
+    className: "grid gap-[21px] mt-[48px] w-[380px]",
   });
   const body = El({
     element: "div",
-    children: [logo],
+    children: [logo, title, form],
     className: "grid w-full justify-center",
   });
   const loginPage = El({
