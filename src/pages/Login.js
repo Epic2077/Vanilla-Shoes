3f44c0f95a6af9458833ff45ede2f6299e410814
Ashkan <the.epic.2077@gmail.com>
2024-11-15 00:20:53 +0330

    Login in progress

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,10 +13,26 @@ export default function login() {
       },
     ],
   });
+  const header = El({
+    element: "div",
+    children: back,
+    className: "w-full h-[56px] items-center grid",
+  });
+
+  const logo = El({
+    element: "img",
+    src: "./src/assets/icons/big-logo.svg",
+    className: "w-[54px] h-[81px] mt-[76px]",
+  });
+  const body = El({
+    element: "div",
+    children: [logo],
+    className: "grid w-full justify-center",
+  });
   const loginPage = El({
     element: "section",
-    children: [back],
-    className: "grid",
+    children: [header, body],
+    className: "grid px-[24px] py-[12px]",
   });
   return loginPage;
 }
