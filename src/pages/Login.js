6803140665b971b411def0085ca27fc05914640e
Ashkan <the.epic.2077@gmail.com>
2024-11-15 00:13:46 +0330

    Login in progress

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,21 @@
 import { El } from "../utils/create-element";
+import { router } from "../routes/router";
 
 export default function login() {
+  const back = El({
+    element: "img",
+    src: "./src/assets/icons/back.svg",
+    className: "cursor-pointer",
+    eventListener: [
+      {
+        event: "click",
+        callback: () => router.navigate("/Home"),
+      },
+    ],
+  });
   const loginPage = El({
     element: "section",
-    children: [],
+    children: [back],
     className: "grid",
   });
   return loginPage;
