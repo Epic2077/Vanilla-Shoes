28e7ddbae9fedd827e67d24c15e7c7ef05049813
Ashkan <the.epic.2077@gmail.com>
2024-11-23 14:55:07 +0330

    Editing Cart.js (Showcasing the products in user's cartlist) -Add Trash Function-

--- src/components/cart-card/cart-card.js
+++ src/components/cart-card/cart-card.js
@@ -46,6 +46,99 @@ export default async function cartCard() {
                   element: "img",
                   src: "../../../src/assets/icons/trash.svg",
                   className: "w-[20px] ml-auto cursor-pointer",
+                  eventListener: [
+                    {
+                      event: "click",
+                      callback: () => {
+                        const remove = El({
+                          element: "div",
+                          children: [
+                            El({
+                              element: "h2",
+                              children: "Remove From Cart?",
+                              className: "text-[24px] font-bold text-black",
+                            }),
+                            El({
+                              element: "hr",
+                              className: "my-4",
+                            }),
+                            El({
+                              element: "div",
+                              children: [
+                                El({
+                                  element: "img",
+                                  src: cart.images,
+                                  className: "w-[140px] rounded-[30px]",
+                                }),
+                                El({
+                                  element: "div",
+                                  children: [
+                                    El({
+                                      element: "div",
+                                      children: [
+                                        El({
+                                          element: "h2",
+                                          children: cart.title,
+                                          className:
+                                            "text-[20px] font-semibold",
+                                        }),
+                                      ],
+                                    }),
+                                    El({
+                                      element: "div",
+                                      children: [
+                                        El({
+                                          element: "div",
+                                          children: [],
+                                          className: `rounded-full bg-${cart.selectedColor}-700 w-[20px] h-[20px] `,
+                                        }),
+                                        El({
+                                          element: "p",
+                                          children: `${cart.selectedColor} | Size = ${cart.selectedSize}`,
+                                          className: "text-[15px] text-gray",
+                                        }),
+                                      ],
+                                      className:
+                                        "flex gap-2 items-center h-max",
+                                    }),
+                                  ],
+                                }),
+                                El({
+                                  element: "div",
+                                  children: [
+                                    El({
+                                      element: "h2",
+                                      children: `$${cart.ttlPrice}.00`,
+                                      className: "text-[20px] font-semibold",
+                                    }),
+                                    El({
+                                      element: "div",
+                                      children: cart.qty,
+                                      className:
+                                        "px-[15px] py-[5px] bg-gray-200 rounded-full",
+                                    }),
+                                  ],
+                                  className: "flex justify-between",
+                                }),
+                              ],
+                              className: "flex gap-4",
+                            }),
+                          ],
+                          className:
+                            "absolute bottom-0 bg-white px-[32px] py-[24px] z-10",
+                        });
+                        const removeLayer = El({
+                          element: "div",
+                          children: [],
+                          className:
+                            "bg-slate-300 opacity-70 w-screen h-screen absolute top-0 ",
+                        });
+                        document.body.appendChild(removeLayer);
+                        document.body.appendChild(remove);
+                        document.body.classList.add("overflow-hidden");
+                      },
+                    },
+                  ],
                 }),
               ],
               className: "flex",
